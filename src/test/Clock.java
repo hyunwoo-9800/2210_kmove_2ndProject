fa045d15faef459babdb83a62f9e1b2e37573c6d
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-01-28 17:23:58 +0900

    [add] JSP 기초_2025/01/28 @1.0h

---
+++ src/test/Clock.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author HW
+ *
+ */
+public class Clock {
+
+	/**
+	 * @param args
+	 */
+	public String now() {
+		// TODO Auto-generated method stub
+
+		SimpleDateFormat f = new SimpleDateFormat("HH시 mm분 ss초");
+		return f.format(new Date());
+	}
+
+}
